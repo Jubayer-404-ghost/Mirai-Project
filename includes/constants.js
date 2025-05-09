971ae029dd3a888683fb8970f41307733c007283
Jubayer-404-ghost <jubayervhau@gmail.com>
2025-05-09 16:44:33 +0000

    Create constants.js

---
+++ includes/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MESSAGES: {
+    noPermission: "You don't have permission to use this command.",
+    unknownCommand: "Unknown command. Try again with a valid command."
+  }
+};
